285184fe65a0faca0ba25f6db69e21a5fcf1767e
Jeff Welling <jeff.welling@gmail.com>
2011-07-25 08:52:41 -0400

    Added some stubs for specs in main_spec.rb

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -19,6 +19,21 @@ describe Tget::Mainn do
 
   it "Should load scrapers into prioritized list"
   it "Should not load scrapers with prio above MAX_PRIO"
+  it "Should produce no output with the --silent option enabled"
+  it "Should show debugging output with --debug option enabled"
+  it "Should read shows from config file"
+  it "Should read config options from config file if they exist"
+  it "Should 'search' a scraper for a show from the config file"
+  it "Should continue searching scrapers sequentially until a match is found"
+  it "Should not explode violently if a scraper cannot be reached"
+  it "Should return matching results from the scraper if they are found, or an empty array"
+  it "Should download to options['download_dir']"
+  it "Should download to options['download_dir'] even when specified as option"
+  it "Should create the download directory if it doesn't exist"
+  it "Should assign a random name appended with '.torrent' if no name provided" #Can't use show name & ep ID because don't have that info when saving file
+  it "Should be able to download torrents with '[' and ']' in the name"
+  it "Should save the show and epID when .torrent has been downloaded"
+  it "Should not re-download shows that are in options['downloaded_files']"
 
 
 end
