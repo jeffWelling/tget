6c3716583c53f14f4c4781125666d92efc64584b
Jeff Welling <jeff.welling@gmail.com>
2011-08-29 23:16:31 -0400

    Added licensing headers to files

--- lib/tget/result.rb
+++ lib/tget/result.rb
@@ -1,3 +1,19 @@
+#    This file is part of tget.
+#    Copyright 2011 Jeff Welling
+#
+#    tget is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    tget is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with tget.  If not, see <http://www.gnu.org/licenses/>.
+#
 module Tget
   #This class is to help turn abitrary data intto a dataset we can use
   #A Result object is created per result returned from the scrapers
